d2cb32885283d6be4a133e5045ab1c837473e2ba
WhiteCells <xuan2692659908@foxmail.com>
2023-01-31 10:43:02 +0800

    Site updated: 2023-01-31 10:43:00

---
+++ js/title.js
@@ -0,0 +1,1 @@
+var titleTime,OriginTitile=document.title;document.addEventListener("visibilitychange",function(){document.hidden?(document.title="(＞﹏＜)别走呀",clearTimeout(titleTime)):(document.title="(=•ω＜=)☆回来了",titleTime=setTimeout(function(){document.title=OriginTitile},2e3))});
