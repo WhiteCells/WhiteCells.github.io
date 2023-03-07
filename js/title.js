0103489a16aac1570ac221a3132706e7cb15226e
WhiteCells <xuan2692659908@foxmail.com>
2023-03-07 13:21:54 +0800

    Site updated: 2023-03-07 13:21:49

--- js/title.js
+++ js/title.js
@@ -1,1 +1,17 @@
-var titleTime,OriginTitile=document.title;document.addEventListener("visibilitychange",function(){document.hidden?(document.title="(＞﹏＜)别走呀",clearTimeout(titleTime)):(document.title="(=•ω＜=)☆回来了",titleTime=setTimeout(function(){document.title=OriginTitile},1e3))});
+//动态标题
+var OriginTitile = document.title;
+var titleTime;
+document.addEventListener('visibilitychange', function () {
+    if (document.hidden) {
+        //离开当前页面时标签显示内容
+        document.title = '(＞﹏＜)别走呀';
+        clearTimeout(titleTime);
+    } else {
+        //返回当前页面时标签显示内容
+        document.title = '(=•ω＜=)☆回来了';
+        //两秒后变回正常标题
+        titleTime = setTimeout(function () {
+            document.title = OriginTitile;
+        }, 1000);
+    }
+});
